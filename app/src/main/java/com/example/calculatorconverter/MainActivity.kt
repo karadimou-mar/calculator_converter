a91f5fcc88b8109a962b5b1da9458b2c5e78f318
karadimou-mar <karadimou.mar@gmail.com>
2019-12-30 21:01:05 +0200

    Changing from fragments to activities

--- app/src/main/java/com/example/calculatorconverter/MainActivity.kt
+++ app/src/main/java/com/example/calculatorconverter/MainActivity.kt
@@ -2,44 +2,33 @@ package com.example.calculatorconverter
 
 
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.Fragment
-import com.google.android.material.bottomnavigation.BottomNavigationView
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item->
-        when(item.itemId){
-            R.id.navigation_calculator -> {
-                println("home pressed")
-                replaceFragment(CalculatorFragment())
-                return@OnNavigationItemSelectedListener true
-            }
-            R.id.navigation_currency -> {
-                println("map pressed")
-                replaceFragment(CurrencyFragment())
-                return@OnNavigationItemSelectedListener true
-            }
-        }
-
-        false
-
-    }
-
+    var hasError = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        bottomNavigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
-        replaceFragment(CalculatorFragment())
+
     }
 
-
-    private fun replaceFragment(fragment: Fragment){
-        val fragmentTransaction = supportFragmentManager.beginTransaction()
-        fragmentTransaction.replace(R.id.fragmentContainer, fragment)
-        fragmentTransaction.commit()
+    fun onNumberClicked(view: View) {
+        if (hasError) {
+            textView.text = (view as Button).text
+            Log.d("XCALCULATOR", "bUTTON PRESSED")
+            hasError = false
+        } else {
+            textView.append((view as Button).text)
+            Log.d("YCALCULATOR", "bUTTON PRESSED")
+        }
+        //handling error
     }
+
 }
